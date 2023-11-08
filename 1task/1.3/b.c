3bda6a0e690dea11893a630f0d672b385b27177c
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-11-08 19:22:27 +0700

    Create b.c

---
+++ 1task/1.3/b.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
+struct ThreadParams {
+    int num;
+    const char *message;
+};
+
+void *mythread(void *arg) {
+    struct ThreadParams *params = (struct ThreadParams *)arg;
+    printf("Thread %d: %s\n", params->num, params->message);
+    pthread_exit(NULL);
+}
+
+int main() {
+    struct ThreadParams *params =
+            (struct ThreadParams *)malloc(sizeof(struct ThreadParams));
+    params->num = 1;
+    params->message = "Hello from thread";
+    pthread_attr_t attr;
+    pthread_t tid;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    if (pthread_create(&tid, &attr, mythread, params) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+    sleep(2);
+    return 0;
+}
