48d1e04575086a828927a706d476f33a9f457138
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-11-22 17:26:47 +0700

    Update a.c

--- 1task/1.6/a.c
+++ 1task/1.6/a.c
@@ -1,4 +1,5 @@
-#include <errno.h>
+#define _GNU_SOURCE
+
 #include <pthread.h>
 #include <sched.h>
 #include <stdio.h>
@@ -17,60 +18,45 @@ typedef pid_t mythread_t;
 // Тип для функции потока
 typedef int (*mythread_start_routine)(void *);
 
-void *mythread(void *arg) {
-  printf("\n--------------------------\n");
-
-  printf("mythread [%d %d %d]\n", getpid(), getppid(), gettid());
-  return NULL;
-}
 
 int mythread_create(mythread_t *thread, mythread_start_routine function,
                     void *arg) {
-  void *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
-                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-  *thread = clone(
-      function, (char *)stack + STACK_SIZE,
-      CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD, arg);
+    *thread = clone(
+            function, (char *)stack + STACK_SIZE,
+            CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD, arg);
 
-  if (*thread == -1) {
-    perror("clone");
-    munmap(stack, STACK_SIZE);
-    return -1;
-  }
+    if (*thread == -1) {
+        perror("clone");
+        munmap(stack, STACK_SIZE);
+        return -1;
+    }
 
-  return 0;
+    return 0;
 }
 
-void aLetter() {
-  pthread_t tid;
-  int err;
-  err = pthread_create(&tid, NULL, mythread, NULL);
-  if (err) {
-    printf("main: pthread_create() failed: %s\n", strerror(err));
-    return;
-  }
-  pthread_join(tid, NULL);  // a
-}
 
 int my_thread_function(void *arg) {
-  printf("Thread message = %s\n", (char *)arg);
-  printf("mythread [%d %d %d]\n", getpid(), getppid(), gettid());
-  //   sleep(2);
-  return 0;
+    printf("Thread message = %s\n", (char *)arg);
+    printf("mythread [%d %d %d]\n", getpid(), getppid(), gettid());
+    //   sleep(2);
+    return 0;
 }
 
 int main() {
-  mythread_t thread;
-  char *message = "I'm here";
-  printf("Main [%d %d %d]!\n", getpid(), getppid(), gettid());
-  if (mythread_create(&thread, my_thread_function, message) != 0) {
-    fprintf(stderr, "Failed to create thread\n");
-    return 1;
-  }
-  int status;
-  waitpid(thread, &status, 0);
-  sleep(2);
-  aLetter();
-  return 0;
+    mythread_t thread;
+    char *message = "I'm here";
+    printf("Main [%d %d %d]!\n", getpid(), getppid(), gettid());
+
+    if (mythread_create(&thread, my_thread_function, message) != 0) {
+        fprintf(stderr, "Failed to create thread\n");
+        return 1;
+    }
+
+    int status;
+    waitpid(thread, &status, 0);
+    sleep(2);
+    return 0;
 }
