a1bda08c4a0022462d02caa69f3e106cfde5e4e7
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-12-20 17:46:19 +0700

    Create queue.h

---
+++ 2task/2.3/queue.h
@@ -0,0 +1,30 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+
+typedef struct _Node {
+    char value[100];
+    struct _Node *next;
+    pthread_mutex_t sync;
+} Node;
+
+typedef struct _Storage {
+    Node *first;
+} Storage;
+
+typedef struct _ThreadData {
+    Storage *storage;
+    int *counter;
+} ThreadData;
+
+
+Storage* initialize_storage(int capacity);
+
+void add_node(Storage *storage, const char *value);
+
+void fill_storage(Storage *storage);
+
+void print_storage(Storage *storage);
